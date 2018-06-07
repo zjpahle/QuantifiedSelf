d3fc1f94040a926b72d3bba0d104fe12a6482dc8
zjpahle <zjpahle@users.noreply.github.com>
2018-06-07 08:25:38 -0700

    fixed loop

--- fitnessGraphingGUI.py
+++ fitnessGraphingGUI.py
@@ -4,7 +4,11 @@ Created on Wed Jun  6 14:36:09 2018
 
 @author: pahlza
 """
-import tkinker as tk
+import tkinter as tk
+from tkinter import ttk
 win = tk.Tk()
 win.title('test#1')
+
+ttk.Label(win, text='A Label').grid(column=0, row=0)
+
 win.mainloop()
